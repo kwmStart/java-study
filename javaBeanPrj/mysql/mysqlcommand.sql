52c0bb25b9ca46771146212c54c624b864991589
kwm <562752110qq.com>
2020-06-02 23:59:43 +0800

    DQL 条件查询2

--- javaBeanPrj/mysql/mysqlcommand.sql
+++ javaBeanPrj/mysql/mysqlcommand.sql
@@ -51,8 +51,98 @@ DESC departments;
 #做字段空处理
 SELECT IFNULL(commission_pct,0) "res",commission_pct FROM employees;
 
-# 条件查询
+
+#进阶二 条件查询
 /*
  语法：
     select 查询列表  from 表名  where 筛选条件
+ 分类：
+    一、按照条件表达式筛选。
+       1）、条件运算符： > , < , = , <> , >= , <=
+    二、按照逻辑表达式筛选。
+       2）、逻辑运算符：&& , || , !    Java使用
+			and , or , not
+			&&和and：并且  两个条件都为true时，结果为true，反之为false。
+			||和or   或者  只要有一个为true，结果就为true，反之为false。
+			！和not   非    如果连接本身为true，结果就为false。
+    三、模糊查询：
+        like
+        between and
+        in
+        is null
+
+*/
+#按照条件表达式筛选。条件运算符： > , < , = , <> , >= , <=
+# 查询工资大于12000的员工信息
+SELECT * FROM employees WHERE salary >= 12000;
+
+# 查询部门编号不等于90号的员工名和部门编号
+SELECT
+    CONCAT(last_name, ' ', first_name) 'name',
+    department_id
+FROM
+    employees
+-- WHERE department_id != 90 ;
+WHERE department_id <> 90 ;
+
+#按照逻辑表达式筛选。
+#1、查询工资在10000到20000之间的员工名和工资以及奖金。
+SELECT
+    lasT_name,
+    salary,
+    commission_pct
+FROM
+    employees
+WHERE salary > 10000
+  AND salary < 20000 ;
+
+#2、查询部门编号不在90到110之间，或者工资高于15000的员工信息
+SELECT * FROM employees WHERE (department_id < 90 OR department_id >110) OR salary >15000;
+
+#按照模糊查询。
+/*
+ like    % 代表统配符
+ 特点：
+ 1、通常和通配符搭配使用
+    通配符：% 任意多个字符，包含0个字符。
+            _ 任意单个字符。
+ between and
+ 1、可以提高SQL的简洁度。
+ 2、包含临界值
+ 3、两个临界值不可以调换顺序，大于等于  小于等于
+ in
+ 1、使用in 比or的SQL简洁度高
+ 2、in列表的值类型必须统一或者兼容
+ 3、不支持通配符
+ is null或者 is not null
+ 1、 =或者<> 不可以判断 null
+*/
+#1、like
+#查询员工名称中包含字符a的员工信息
+SELECT * FROM employees WHERE last_name LIKE '%a%';
+#查询员工名称第三个字符为e，第五个字符为a的员工名和工资
+SELECT last_name ,salary FROM employees WHERE last_name LIKE '__n_l%';
+#查询员工名中第二个字符为_ 的员工名   _需要转义 "\_"
+SELECT last_name ,salary FROM employees WHERE last_name LIKE '_\_%';
+#escape 指定转义字符  即指定“$”为转义字符 'S_'等于于'\_'
+SELECT last_name ,salary FROM employees WHERE last_name LIKE '_$_%' ESCAPE '$';
+
+#2 between and
+#查询部门编号在100到120之间的员工信息
+SELECT * FROM employees WHERE employee_id BETWEEN 100 AND 120;
+
+#3 in
+# 查询员工的工种编号是 IT_PROG 、 AD_VP 中的任一一个的员工的名和工种编号
+SELECT last_name ,job_id FROM employees WHERE job_id IN ('IT_PROG','AD_VP');
+
+#is null
+# 查询没有奖金的员工信息
+SELECT * FROM employees WHERE commission_pct IS NULL;
+#安全等于 <=>  不仅可以判断空值null  也可以判断数值
+SELECT * FROM employees WHERE commission_pct <=> NULL;
+# 查询员工工资为12000的员工信息
+SELECT * FROM employees WHERE salary <=> 12000;
+
+/*
+安全等于 <=>  不仅可以判断空值null  也可以判断数值  可读性差
 */
