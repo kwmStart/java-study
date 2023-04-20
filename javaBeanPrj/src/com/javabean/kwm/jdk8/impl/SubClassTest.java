554b83575887c9da763406d6161856d2ac1b0580
kwmStart <562752110@qq.com>
2023-04-20 21:35:33 +0800

    增加实现类实现多个接口的同名同参默认方法测试。

--- javaBeanPrj/src/com/javabean/kwm/jdk8/impl/SubClassTest.java
+++ javaBeanPrj/src/com/javabean/kwm/jdk8/impl/SubClassTest.java
@@ -1,6 +1,7 @@
 package com.javabean.kwm.jdk8.impl;
 
 import com.javabean.kwm.jdk8.CompareA;
+import com.javabean.kwm.jdk8.CompareB;
 
 /**
  * @ClassName SubClassTest
@@ -21,19 +22,28 @@ public class SubClassTest {
         // 知识点2：通过实现类的对象，可以调用接口中的默认方法。
         // 如果实现类重写了接口中的默认方法，调用时 ，仍然是调用重写以后的方法。
         subClass.method2();
-//        SubClass.method2();
 
         // 知识点3： 如果子类（实现类）继承的父类和实现的接口中声明了同名同参数的方法，
         // 那么子类在没有重写此方法的情况下，默认调用的时父类中的同名同参数的方法。 --> 类优先原则
+        // 知识点4： 如果实现类实现了多个接口，而这多个接口中定义了同名同参数的默认方法，
+        // 那么在实现类没有重写此方法的情况下，报错 -->接口冲突。
+        // 这就需要我们必须重写此方法。
         subClass.method3();
     }
 
 }
 
-class SubClass implements CompareA {
+class SubClass extends SuperClass implements CompareA, CompareB {
 
     @Override
     public void method2() {
         System.out.println("SubClass: 我是子类实现的接口中的默认方法method2的重写");
     }
+
+    @Override
+    public void method3() {
+        System.out.println("SubClass: 我是实现类中的method3方法，我是重写的默认方法");
+    }
+
+
 }
