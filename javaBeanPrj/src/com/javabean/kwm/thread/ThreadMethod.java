1989b07b399f5f6e735d8d7861e86e9a851de4bf
kwmStart <562752110@qq.com>
2023-05-05 21:47:00 +0800

    线程优先级的设置。

--- javaBeanPrj/src/com/javabean/kwm/thread/ThreadMethod.java
+++ javaBeanPrj/src/com/javabean/kwm/thread/ThreadMethod.java
@@ -14,6 +14,19 @@ package com.javabean.kwm.thread;
  * 9、sleep(long milliTime) 单位毫秒； 让当前线程“睡眠”指定的milliTime毫秒，在指定的milliTime毫秒时间内，当前线程是阻塞状态。
  * 10、isAlive()： 判断当前线程是否存活
  *
+ *
+ * 线程优先级
+ * 1、
+ *   MIN_PRIORITY=1;
+ *   NORM_PRIORITY=5;  -- 默认的优先级
+ *   MAX_PRIORITY=10;
+ * 2、如何获取和设置当前线程的优先级
+ *   getPriority() 获取当前线程的优先级
+ *   setPriority(int p) 设置当前线程的优先级
+ *
+ *   说明： 高优先级的线程要抢占低优先级的线程CPU的执行权。但只是从概率上抢占（即获取执行权的优先级比较高），即高优先级的线程高概率的
+ *   情况下会被CPU执行，但并不意味着只能当高优先级的线程执行完成以后，低优先级的线程才可以被执行。
+ *
  * @Author kwm
  * @Date 2023/5/5 20:01
  */
@@ -23,11 +36,12 @@ class MethodThread extends Thread {
         for (int i = 0; i < 100; i++) {
 
             // 设置睡眠时间
+            /* todo 测试线程优先级 注释掉
             try {
                 sleep(100);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
-            }
+            }*/
 
             if (i % 2 ==  0) {
                 System.out.println(Thread.currentThread().getName() + " : " + i);
@@ -61,6 +75,7 @@ public class ThreadMethod {
 
         MethodThread thread1 = new MethodThread();
         thread1.setName("线程一");
+        thread1.setPriority(Thread.MAX_PRIORITY);
         thread1.start();
 
         // 调用含参构造器
@@ -73,19 +88,20 @@ public class ThreadMethod {
 
         // 给主线程设置名称
         Thread.currentThread().setName("主线程");
-
+        Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
         for (int i = 0; i < 100; i++) {
             if (i % 2 ==  0) {
                 System.out.println(Thread.currentThread().getName() + " : " + i);
             }
 
+            /* todo 测试线程优先级 注释掉
             if (i == 20 ) {
                 try {
                     thread1.join();
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-            }
+            }*/
         }
 
         // 测试当前线程 线程名称
