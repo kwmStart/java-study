989990586450543de50642dc22a3ea3a901b4aac
kwmStart <562752110@qq.com>
2023-05-08 22:35:01 +0800

    线程的创建方式二。通过实现Runnable接口方式创建。

---
+++ javaBeanPrj/src/com/javabean/kwm/thread/ThreadCreate1Test.java
@@ -0,0 +1,51 @@
+package com.javabean.kwm.thread;
+
+/**
+ * 多线程的创建：方式二：实现Runnable接口
+ * 1、创建一个实现Runnable接口的类
+ * 2、实现类去实现Runnable接口的抽象方法：run()
+ * 3、创建实现类的对象
+ * 4、将此对象作为参数传递到Thread类的构造器中，创建Thread类的对象
+ * 5、通过Thread类的对象调用start()
+ *
+ *
+ *
+ * @ClassName ThreadCreate1Test
+ * @Description 多线程创建方式二练习
+ * @Author kwm
+ * @Date 2023/5/8 21:58
+ */
+// 1、创建一个实现Runnable接口的类
+class MThread implements Runnable {
+//    2、实现类去实现Runnable接口的抽象方法：run()
+    @Override
+    public void run() {
+        for (int i = 0;i <= 100;i++){
+            if (i % 2 == 0){
+                System.out.println(Thread.currentThread().getName() + ":" +i);
+            }
+        }
+    }
+}
+
+public class ThreadCreate1Test {
+
+    public static void main(String[] args) {
+        // 3、创建实现类的对象
+        MThread mThread = new MThread();
+
+        // 4、将此对象作为参数传递到Thread类的构造器中，创建Thread类的对象
+        Thread t1 = new Thread(mThread);
+        t1.setName("线程一");
+        // 5、通过Thread类的对象调用start() ①启动线程 ②调用当前线程的run()
+        t1.start();
+
+        // 重新再启动一个线程，遍历100以内的整数
+        Thread t2 = new Thread(mThread);
+        t2.setName("线程二");
+        t2.start();
+    }
+
+}
+
+
